bb7844c296a5ea8837d4158a0de004c3216e55d4
jk-rishav-raj <92789056+jk-rishav-raj@users.noreply.github.com>
2021-11-01 14:17:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ const request = require("request");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
@@ -18,13 +17,31 @@ app.post("/", function(req, res) {
   const amount = req.body.Amount;
   const from = req.body.From;
   const API_KEY = "eacc1e233ba83770eb9853ec57e72f6c";
-  const url = "http://data.fixer.io/api/latest?access_key=" + API_KEY;
+  const url = "http://data.fixer.io/api/latest?access_key=" + API_KEY ;
 
   request(url, function(error, response, body) {
+    
+    if(error) {
+        console.log(error);
+        return ;
+    }
+    
+    console.log(response.statusCode);
 
     const data = JSON.parse(body);
-    const rate = data.rates[to];
-    const exchangeAmount = rate * amount;
+
+    console.log(data);
+    
+    const toRate = data.rates[to];
+    const fromRate = data.rates[from];
+
+    if(!toRate || !fromRate) {
+        res.write("Invalid Currency");
+        res.send();
+        return ;
+    }
+
+    const exchangeAmount = amount * toRate / fromRate;
 
     res.write("<h1> " + amount + " " + from + " = " + exchangeAmount + " " + to + "</h1>");
     res.send();
